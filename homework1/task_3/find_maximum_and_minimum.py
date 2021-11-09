65edbf45518ac343f6fecdab8866e340f82c4ec1
Anna Marashova <marashova.anna@yandex.ru>
2021-11-09 17:06:05 +0300

    Task 3 with black and isort

--- homework1/task_3/find_maximum_and_minimum.py
+++ homework1/task_3/find_maximum_and_minimum.py
@@ -3,9 +3,9 @@ from typing import Tuple
 
 def find_maximum_and_minimum(file_name: str) -> Tuple[int, int]:
     with open(file_name) as file:
-        lines = [int(line.rstrip('\n')) for line in file]
+        lines = [int(line.rstrip("\n")) for line in file]
         if len(lines) < 2:
-            raise ValueError('File must contain at least 2 lines with numbers')
+            raise ValueError("File must contain at least 2 lines with numbers")
         lines.sort()
         min_max = []
         min_max.append(lines[0])
